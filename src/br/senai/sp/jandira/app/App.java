05f4ab519465aea502d07d62598d4d68519764ce
LEONARDO BOMFIM ANTUNES <21276366@CFP127ALUNO.LOCAL>
2021-09-30 16:51:05 -0300

    adicionou o contrutor e o metodo transferir da classe conta

--- src/br/senai/sp/jandira/app/App.java
+++ src/br/senai/sp/jandira/app/App.java
@@ -7,37 +7,44 @@ public class App {
 	public static void main(String[] args) {
 		
 		// Criação da conta da Maria
-		Conta contaMaria = new Conta();
+		Conta contaMaria = new Conta("7845-8");
 		contaMaria.titular = "Maria Antonieta";
-		contaMaria.numero = "7845-8";
 		contaMaria.numeroAgencia = "4214-9";
-		contaMaria.saldo = 500.0;
+		contaMaria.depositar(500);
 		contaMaria.tipo = "Corrente";
 		
 		// Criação da conta do Pedro
-		Conta contaPedro = new Conta();
+		Conta contaPedro = new Conta("6547-6");
 		contaPedro.titular = "Pedro Cabral";
-		contaPedro.numero = "6547-6";
-		contaPedro.saldo = 200.0;
+		contaPedro.depositar(200);
 		contaPedro.tipo = "Poupança";
 		contaPedro.numeroAgencia = "4214-9";
 		
 		// Criação da conta da Ana
-		Conta contaAna = new Conta();
+		Conta contaAna = new Conta("2145-9");
 		contaAna.titular = "Ana Gomes";
-		contaAna.numero = "23145-9";
-		contaAna.saldo = 2000.0;
+		contaAna.depositar(2000);
 		contaAna.tipo = "Corrente";
 		contaAna.numeroAgencia = "4214-9";
 		
-		
 		// Exibir os detalhes das contas
+		// contaMaria.exibirDetalhes();
+		// contaPedro.exibirDetalhes();
+		// contaAna.exibirDetalhes();
+		
+		contaMaria.depositar(500);
+		
+		// contaMaria.sacar(100);
+		
+		// contaMaria.exibirDetalhes();
+		
+		
+		contaMaria.transferir(contaPedro, 200);
+		
 		contaMaria.exibirDetalhes();
 		contaPedro.exibirDetalhes();
-		contaAna.exibirDetalhes();
 		
 		
-
 	}
 
 }
--- src/br/senai/sp/jandira/model/Conta.java
+++ src/br/senai/sp/jandira/model/Conta.java
@@ -1,33 +1,81 @@
 package br.senai.sp.jandira.model;
 
 public class Conta {
-	
 	public String tipo;
-	public String numero;
 	public String numeroAgencia;
 	public String titular;
-	public double saldo;
+	private String numero;
+	private double saldo;
+	
+	public Conta(String numero) {
+		this.numero = numero;
+		this.tipo = tipo;
+	}
 	
 	public void depositar(double valorDeposito) {
-		saldo += valorDeposito;
-	}
-	
-	public void sacar(double valorSaque) {
-		saldo -= valorSaque;
-	}
-	
-	public void transferir() {
+		//System.out.println("Depositando R$" + valorDeposito + " na "
+		//				+ "\nconta de " + titular + "...\n");
 		
+		if (valorDeposito < 0) {
+			System.out.println("Valor incorreto!"
+							+ "\nnão foi possivel efetuar o"
+							+ "\ndeposito.");
+			
+		} else {
+			saldo += valorDeposito;
+			
+			//System.out.println("Deposito realizado com \nsucesso\n");
+		}
+		
+		//System.out.println();
+	}
+	
+	public boolean sacar(double valorSaque) {
+		System.out.println("Sacando R$" + valorSaque + " na conta "
+						+ "\nde " + titular + "...\n");
+		
+		if (valorSaque < 0) {
+			System.out.println("Valor incorreto!"
+							+ "\nnão foi possivel efetuar o"
+							+ "\nsaque.\n");
+			return false;
+		} else if (valorSaque > saldo) {
+			System.out.println("Valor incorreto!"
+							+ "\nsaldo insuficiente.\n");
+			return false;
+		} else {
+			saldo -= valorSaque;
+
+			System.out.println("Saque realizado com \nsucesso\n");
+			return true;
+		}
+		
+
+	}
+	
+	public void transferir(Conta contaDestino, double valorTransferencia) {
+		System.out.println("Transferindo R$" + valorTransferencia + " da "
+				+ "\nconta de " + titular + "\npara a conta de \n"+ contaDestino.titular + "...\n");
+		
+		boolean resultado = sacar(valorTransferencia);
+		
+		if (resultado) {
+			contaDestino.depositar(valorTransferencia);
+		} else {
+			System.out.println("Impossivel realizar \ntransferencia!\n");
+		}
 	}
 	
 	public void exibirDetalhes() {
-		System.out.println();
-		System.out.println("---------------------");
+		System.out.println("--------------------------");
 		System.out.printf("Titular: %s\n", titular);
 		System.out.printf("Número: %s\n", numero);
 		System.out.printf("Agência: %s\n", numeroAgencia);
 		System.out.printf("Tipo: %s\n", tipo);
 		System.out.printf("Saldo: %s\n", saldo);
+		System.out.println("--------------------------\n");
 	}
-	
 }
+
+
+
